fbb25ba412e3724312fd8c90b8bb297a8b25d53f
TrickyPR <trickypr@icloud.com>
2022-10-23 13:49:40 +1100

    ⬆️ Update Octokit

--- repos.ts
+++ repos.ts
@@ -2,17 +2,17 @@ import { UpdateCheckerConfig } from './src/bot.js'
 
 export const config: UpdateCheckerConfig[] = [
   {
-    assignedUsers: ['trickypr', 'pressjump'],
-    branch: 'alpha',
-    issueLabel: 'upstream',
-    name: 'Pulse Browser',
-    repo: 'pulse-browser/browser',
-  },
-  {
     assignedUsers: ['trickypr'],
     branch: 'main',
     issueLabel: 'upstream',
     name: 'Quark Runtime',
     repo: 'quark-platform/runtime',
+  },
+  {
+    assignedUsers: ['trickypr', 'pressjump'],
+    branch: 'alpha',
+    issueLabel: 'upstream',
+    name: 'Pulse Browser',
+    repo: 'pulse-browser/browser',
   },
 ]
